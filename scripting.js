8fc5fee64a206286bcc275ccf3acc6a1d42f6b0c
Dev1121 <helloworld@email-skyminestudios.ml>
2022-06-02 13:16:50 +0800

    Add files via upload

---
+++ scripting.js
@@ -0,0 +1,70 @@
+var getName = prompt("Nama warnet mu apa ? ");
+
+
+if (getName === null || !isNaN(getName)){
+    while (getName !== null || isNaN(getName)){
+        alert("Invalid , please try again");
+        getName = prompt("Siapa namamu ? ");
+        console.log(getName)
+       if (getName !== null || !isNaN(getName)){
+           document.write();
+           break;
+       }
+    }
+}else {
+    document.write();
+}
+
+var color = prompt("Mau Warna Apa ? Ketik Warna HTML nya di Gugel contoh #fffff ");
+
+
+if (getName === null || !isNaN(getName)){
+    while (getName !== null || isNaN(getName)){
+        alert("Invalid , please try again");
+        getName = prompt("Siapa namamu ? ");
+        console.log(getName)
+       if (getName !== null || !isNaN(getName)){
+           document.write("Halo ", getName);
+           break;
+       }
+    }
+
+}else {
+    document.write();
+}
+
+var brtag = prompt("Line Ke 2 / < br> tag, Klik Cancel Jika tidak Mau.");
+
+
+if (getName === null || !isNaN(getName)){
+    while (getName !== null || isNaN(getName)){
+        alert("Invalid , please try again");
+        getName = prompt("Siapa namamu ? ");
+        console.log(getName)
+       if (getName !== null || !isNaN(getName)){
+           document.write("Halo ", getName);
+           break;
+       }
+    }
+
+}else {
+    document.write();
+}
+
+var done = alert("Udah Selesai Nih:)");
+
+
+if (getName === null || !isNaN(getName)){
+    while (getName !== null || isNaN(getName)){
+        alert("Invalid , please try again");
+        getName = prompt("Siapa namamu ? ");
+        console.log(getName)
+       if (getName !== null || !isNaN(getName)){
+           document.write("Halo ", getName);
+           break;
+       }
+    }
+
+}else {
+    document.write("Copy Lalu Paste Ke Warnet life : ");
+}
